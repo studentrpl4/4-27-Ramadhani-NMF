efe17a7f9308f00488928f1f5ba96bef48d458f3
unknown <3103120190@student.smktelkom-pwt.sch.id.com>
2022-02-13 13:29:11 +0700

    Upload modul 4

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -13,7 +14,9 @@ class ContactController extends Controller
      */
     public function index()
     {
-        //
+        return view('contacts',[
+            'title' => 'Contacts'
+        ]);
     }
 
     /**
@@ -34,7 +37,12 @@ class ContactController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request->all());
+        $contact = Contact::create($request->all());
+        $contact->save();
+
+        return redirect('contacts');
+
     }
 
     /**
